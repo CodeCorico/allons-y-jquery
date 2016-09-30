94b4379de0081e0b4d8512d32fb9cecd2dd1c58c
Xavier Boubert <xavier@boubert.fr>
2016-09-30 12:16:49 +0200

    feat(jquery): create the feature

---
+++ features/jquery/jquery-gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function($gulp) {
+  $gulp.task('jquery', function(done) {
+    $gulp.src([
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/jquery/dist/jquery.min.map'
+    ])
+      .pipe($gulp.dist('vendor'))
+      .on('end', done);
+  });
+
+  return 'jquery';
+};

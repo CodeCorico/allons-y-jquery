9f2dd33c33be174637bff2461a8feae9b0ca9999
Xavier Boubert <xavier@boubert.fr>
2016-10-19 11:50:29 +0200

    fix(jquery gulpfile): add relative path to $gulp.src()

--- features/jquery/jquery-gulpfile.js
+++ features/jquery/jquery-gulpfile.js
@@ -3,9 +3,9 @@
 module.exports = function($gulp) {
   $gulp.task('jquery', function(done) {
     $gulp.src([
-      'node_modules/jquery/dist/jquery.js',
-      'node_modules/jquery/dist/jquery.min.js',
-      'node_modules/jquery/dist/jquery.min.map'
+      './node_modules/jquery/dist/jquery.js',
+      './node_modules/jquery/dist/jquery.min.js',
+      './node_modules/jquery/dist/jquery.min.map'
     ])
       .pipe($gulp.dist('vendor'))
       .on('end', done);
